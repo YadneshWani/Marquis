574cd7ec3e336559575000fb68d5ed0b95545a15
YadneshWani <yadneshwani0987@gmail.com>
2023-02-06 16:30:13 +0530

    Changes in Home tab

--- Components/NoticeBoard.js
+++ Components/NoticeBoard.js
@@ -1,53 +1,82 @@
-import {useState,useEffect} from "react";
-import {Text,View,StyleSheet,ActivityIndicator,FlatList} from 'react-native'
+import { useState, useEffect } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  ActivityIndicator,
+  FlatList,
+} from "react-native";
 
 import { getNoticeData } from "../Services/NoticeRequest";
-const NoticeBoard=()=>{
-
-    const[noticeArray,setNoticeArray] = useState([]);
-    async function getData(){
-        const noticeData = await getNoticeData();
-        setNoticeArray(noticeData.data);
-        console.log(noticeData);
-    }
-    useEffect(()=>{
-        //   getHomeFeedData();
-        getData();
-    },[]);
-    return(
-        <View>
-            {
-            noticeArray ? 
-            
-            <FlatList
-                data={noticeArray}
-                renderItem={({ item, index }) => (
+const NoticeBoard = () => {
+  const [noticeArray, setNoticeArray] = useState([]);
+  async function getData() {
+    const noticeData = await getNoticeData();
+    setNoticeArray(noticeData.data);
+    console.log(noticeData);
+  }
+  useEffect(() => {
+    //   getHomeFeedData();
+    getData();
+  }, []);
+  return (
+    <View>
+      {/* {noticeArray ? (
+        <FlatList
+          data={noticeArray}
+          renderItem={({ item, index }) => (
             <View style={styles.NoticeContainer}>
-                <Text style={{color:'#585858',marginLeft:8}}>{item.description}</Text>
+              <Text
+                style={{
+                  color: "#585858",
+                  marginLeft: 8,
+                  marginRight: 10,
+                  marginTop: 10,
+                }}
+                horizontal={true}
+              >
+                {item.description}
+              </Text>
             </View>
-                )}
-                keyExtractor={(item, index) => index}
-            />
-           : <ActivityIndicator size={"small "} />
-                }
+          )}
+          keyExtractor={(item, index) => index}
+        />
+      ) : (
+        <ActivityIndicator size={"small "} />
+      )} */}
 
-        </View>
-    )
-}
-const styles=StyleSheet.create({
-    NoticeContainer:{
-        backgroundColor:"#FFF4D2",
-        width:352,
-        height:77,
-        borderRadius:12,
-        alignItems:"center",
-        margin:16,
-        marginTop:8,
-        borderWidth:1,
-        borderColor:"#CABE9B"
-
-   },
-
-
-})
+      {noticeArray ? (
+        noticeArray.map((item, index) => (
+          <View style={styles.NoticeContainer}>
+            <Text
+              style={{
+                color: "#585858",
+                marginLeft: 8,
+                marginRight: 10,
+                marginTop: 10,
+              }}
+            >
+              {item.description}
+            </Text>
+          </View>
+        ))
+      ) : (
+        <ActivityIndicator size={"small "} />
+      )}
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  NoticeContainer: {
+    backgroundColor: "#FFF4D2",
+    width: 352,
+    height: 77,
+    borderRadius: 12,
+    alignItems: "center",
+    margin: 16,
+    marginTop: 8,
+    borderWidth: 1,
+    borderColor: "#CABE9B",
+  },
+});
 export default NoticeBoard;
